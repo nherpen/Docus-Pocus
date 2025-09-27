f0af1be8110b7a21eb040eed2128622c2697abb5
Noud van Herpen <noudvanherpen94@gmail.com>
2025-09-27 15:40:09 +0200

    create statement of work from template

---
+++ docs/230180-PDGF/sow_230180-PDGF.md
@@ -0,0 +1,33 @@
+# Statement of work
+## [Title]
+
+Table: Summary
+| Project Name | [Insert here] |
+|--------------|--------------------------|
+| Customer     | |
+| Author       | |
+| Date         | |
+| Version      | |
+| Status       | |
+
+## Changelog
+
+Table: Changelog
+| Version | Status | Date | Author | Change Summary |
+|---------|--------|------|--------|----------------|
+| 0.1     |        |      |        |                |
+
+## Definitions & Abbreviations
+
+## Introduction
+
+## Services & Deliverables
+
+## Requirements
+
+Table: Requirements
+| ID | Requirement | Verification Method |
+|----|-------------|---------------------|
+|    |             |                     |
+
+## Appendices
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import sys
+import shutil
+from pathlib import Path
+
+SELECT_OPTION_STRING = f"{20*'='}\nPlease select an option [1-n]\n > "
 
 class Main():
     def __init__(self):
@@ -12,13 +16,13 @@ class Main():
             retval = rv_temp
 
     def welcome(self):
-        print("\nWelcome to Docus Pocus")
+        print(f"\nWelcome to Docus Pocus\n{20*'='}")
         print(
             " 1. Create document\n" \
             " 2. Check test coverage\n" \
             " q. Quit"
         )
-        match input("Please select an option [1-n]\n > "):
+        match input(SELECT_OPTION_STRING):
             case "1":
                 return self.create_document
             case "q":
@@ -38,15 +42,33 @@ class Main():
             " 9. Instructions & procedures\n" \
             " q. Back"
         )
-        match input(f"{20*'='}\nPlease select an option [1-n]\n > "):
+        match input(SELECT_OPTION_STRING):
             case "1":
-                print("Very nice")
-                return self.create_document
+                return self.create_sow
             case "q":
                 return self.welcome
             case _:
                 print("Try again")
                 return self.create_document
+    
+    def create_sow(self):
+        print(f"\nCreate Statement of Work\n{20*'='}")
+        src_file = Path("./templates/statement_of_work.md")
+        docs_dir = Path.cwd() / Path("docs")
+
+        projects = [x for x in docs_dir.iterdir() if x.is_dir()]
+        [print(f" {i}. {p.name}") for i, p in enumerate(projects)]
+        proj = input(SELECT_OPTION_STRING)
+
+        try:
+            dst_file = projects[int(proj)] / Path(f"sow_{projects[int(proj)].name}.md")
+        except:
+            print("Try again")
+            return self.create_sow
+
+        shutil.copy(src_file, dst_file)
+        return self.welcome
+        
 
     def exit(self):
         sys.exit()
